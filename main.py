d19616c1a1ab091b8ae79a24080138f4214b1e80
Olga Komar <komar_ol@mail.ru>
2023-07-10 08:12:41 +0300

    added solution to task 22

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # возрастания все те числа, которые встречаются в обоих наборах.
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества. m — кол-во элементов второго множества.
 # Затем пользователь вводит сами элементы множеств.
+
+import random
+
+print(array_n := [random.randint(0, 15) for _ in range(int(input('n= ')))])
+print(array_m := [random.randint(0, 5) for _ in range(int(input('m= ')))])
+
+set_n = set(array_n)
+set_m = set(array_m)
+
+print(result_set := set_n.intersection(set_m))
